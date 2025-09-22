4af276c21510ac85a79effaed735b06dfb3222a6
Kristina <rodkina.kristina2018@yandex.ru>
2025-09-22 15:00:37 +0300

    Merge remote-tracking branch 'origin/master'

---
+++ src/lab3/RandomDouble.java
@@ -0,0 +1,32 @@
+package lab3;
+
+import java.util.Collections;
+import java.util.Random;
+import java.util.ArrayList;
+
+public class RandomDouble {
+    public static void main(String[] args) {
+        ArrayList<Double> lst1 = new ArrayList<>();
+        ArrayList<Double> lst2 = new ArrayList<>();
+
+        Random rand = new Random();
+        for (int i = 0; i < 100; i++) {
+            double rand_int1 = rand.nextDouble(100);
+            lst1.add(rand_int1);
+        }
+
+        System.out.println("Лист test1 до сортировки - " + lst1);
+        Collections.sort(lst1);
+        System.out.println("Лист test1 после сортировки - " + lst1);
+
+        for (int i = 0; i < 100; i++) {
+            Double x = Math.random()*100;
+            lst2.add(x);
+        }
+
+        System.out.println();
+        System.out.println("Лист test2 до сортировки - " + lst2);
+        Collections.sort(lst2);
+        System.out.println("Лист test2 после сортировки - " + lst2);
+    }
+}
