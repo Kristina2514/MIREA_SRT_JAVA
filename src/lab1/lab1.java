761a02e0312ab532b0cef9d4c045ea671aa83828
Kristina <rodkina.kristina2018@yandex.ru>
2025-09-22 14:23:36 +0300

    Initial commit

---
+++ src/lab1/lab1.java
@@ -0,0 +1,109 @@
+//// практическая 1 номер 3
+//public class Main {
+//    public static void main(String[] args) {
+//        int[] arr = {1, 2, 3, 4, 5, 6};
+//        int sum = 0;
+//        for (int i = 0; i < arr.length; i++){
+//            sum += arr[i];
+//        }
+//        float s = (float) sum/arr.length;
+//        System.out.println(sum);
+//        System.out.println(s);
+//    }
+//}
+
+
+////1.4
+//import java.util.Scanner;
+//
+//public class Main {
+//    public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//        int[] arr = new int[5];
+//        int i = 0;
+//        int sm1 = 0;
+//        do {
+//            arr[i] = sc.nextInt();
+//            sm1 += arr[i];
+//            i++;
+//        } while (i < 5);
+//
+//        int j = 0;
+//        int sm2 = 0;
+//        while (j < arr.length) {
+//            sm2 += arr[j];
+//            j++;
+//        }
+//
+//        int mx = -1000000000;
+//        int k = 0;
+//        while (k < arr.length) {
+//            if (arr[k] > mx) {
+//                mx = arr[k];
+//            }
+//            k++;
+//        }
+//
+//        int mn = 100000000;
+//        int f = 0;
+//        do {
+//            if (arr[f] < mn) {
+//                mn = arr[f];
+//            }
+//            f++;
+//        } while (f < arr.length);
+//    System.out.println(sm1);
+//    System.out.println(sm2);
+//    System.out.println(mx);
+//    System.out.println(mn);
+//    }
+//}
+
+////1.5
+//import java.util.Scanner;
+//
+//public class Main {
+//    public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//        int[] arr = new int[5];
+//        for (int i=0; i < arr.length; i++) {
+//            arr[i] = sc.nextInt();
+//            System.out.println(arr[i]);
+//        }
+//    }
+//}
+
+////1.6
+//public class Main {
+//    public static void main(String[] args) {
+//        System.out.println("Вывод первых 10 чисел гармонического ряда");
+//        for (int n = 1; n <= 10; n++) {
+//            double num = (1.0) / n;
+//            System.out.println(n + " число: " + num);
+//        }
+//    }
+//}
+
+////1.7
+//import java.util.Scanner;
+//
+//public class Main {
+//    public static long calc(int num) {
+//        long fact = 1;
+//        for (int i = 1; i <= num; i++) {
+//            fact *= i;
+//        }
+//        return fact;
+//    }
+//
+//
+//    public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//        System.out.println("ТЕСТИРОВАНИЕ МЕТОДА");
+//        System.out.println("Ввести целое неотрицательное число:");
+//        int number = sc.nextInt();
+//        long factor = calc(number);
+//        System.out.println(number + ": " + factor);
+//    }
+//}
+
---
+++ src/lab2/ShopTest.java
@@ -0,0 +1,60 @@
+package lab2;
+
+import java.util.Scanner;
+
+public class ShopTest {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Введите максимальное количество компьютеров в магазине: ");
+        int maxSize = scanner.nextInt();
+        scanner.nextLine();
+
+        Shop shop = new Shop(maxSize);
+
+        System.out.println("    Магазин компьютеров (вместимость: " + maxSize + ")    ");
+
+        boolean running = true;
+        while (running) {
+            System.out.println("Выберите действие:");
+            System.out.println("1. Добавить компьютер");
+            System.out.println("2. Удалить компьютер");
+            System.out.println("3. Найти компьютер");
+            System.out.println("4. Показать все компьютеры");
+            System.out.println("5. Показать сколько есть/нет свободных мест");
+            System.out.println("6. Выйти");
+            System.out.print("Ваш выбор: ");
+
+            int choice = scanner.nextInt();
+            scanner.nextLine();
+
+            switch (choice) {
+                case 1:
+                    shop.addComputer();
+                    break;
+                case 2:
+                    shop.removeComputer();
+                    break;
+                case 3:
+                    shop.findComputers();
+                    break;
+                case 4:
+                    shop.listComputers();
+                    break;
+                case 5:
+                    System.out.println("Компьютеров в магазине: " + shop.getComputerCount());
+                    System.out.println("Свободных мест: " + (shop.getCapacity() - shop.getComputerCount()));
+                    break;
+                case 6:
+                    running = false;
+                    System.out.println("Готово.");
+                    break;
+                default:
+                    System.out.println("Такого варианта нет");
+            }
+        }
+
+        scanner.close();
+        shop.closeScanner();
+    }
+}
