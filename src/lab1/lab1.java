73daa850206023a34249816637143fb8fd4e940a
Kristina <rodkina.kristina2018@yandex.ru>
2025-12-13 16:44:36 +0300

    Merge remote-tracking branch 'origin/master'

--- src/lab1/lab1.java
+++ src/lab1/lab1.java
@@ -1,5 +1,7 @@
 //// практическая 1 номер 3
-//public class Main {
+package lab1;
+
+//public class lab1 {
 //    public static void main(String[] args) {
 //        int[] arr = {1, 2, 3, 4, 5, 6};
 //        int sum = 0;
@@ -59,16 +61,12 @@
 //    }
 //}
 
-////1.5
-//import java.util.Scanner;
-//
-//public class Main {
+////1.5 - ПЕРЕДЕЛАТЬ ! (с аргументами)
+
+//public class lab1 {
 //    public static void main(String[] args) {
-//        Scanner sc = new Scanner(System.in);
-//        int[] arr = new int[5];
-//        for (int i=0; i < arr.length; i++) {
-//            arr[i] = sc.nextInt();
-//            System.out.println(arr[i]);
+//        for (int i=0; i < args.length; i++) {
+//            System.out.println("Аргумент " + (i + 1) + ": " + args[i]);
 //        }
 //    }
 //}
